66f58e1c61add2a192553841a984a913187208a8
Ashfi <ashfipathan@gmail.com>
2020-04-16 21:13:18 -0400

    Created and configured socket

--- main.cpp
+++ main.cpp
@@ -13,15 +13,39 @@
 
 using namespace std;
 
-
 void ping(struct sockaddr *dst) {
- cout << "Made it to the ping function \n";
+
+    // Get raw socket descriptor
+    int socketfd =  socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+    if (socketfd == -1) {
+        cout << "Error when creating socket descriptor. \n";
+        return;
+    } 
+
+    int ttlValue = 64;
+    int msg_count = 0;
+    long rttMSec = 0;
+    
+    struct timespec time_start, time_end, tfs, tfe;
+    struct timeval timeOut;
+    timeOut.tv_sec = 1;
+    timeOut.tv_usec = 0;
+
+    // Configuring socket to use TTL in the IP header
+    int socketOpt = setsockopt(socketfd, IPPROTO_IP, IP_TTL, &ttlValue, sizeof(ttlValue));
+    if (socketOpt == -1) {
+        cout << "Unable to configure socket. \n";
+    }
+
+    // Configure socket to timeout on receiving
+    setsockopt(socketfd, SOL_SOCKET, SO_RCVTIMEO,(const char*) &timeOut, sizeof(timeOut));
+
 }
 
 int main(int argc, char *argv[])
 {
     if (argc != 2) {
-        printf("usage: ./ping <address>\n");
+        cout << "usage: ./ping <address> \n";
         return 1;
     }
     struct addrinfo *infoptr;
@@ -30,7 +54,7 @@ int main(int argc, char *argv[])
         cout << "Unable to connect to given address. \n";
         return 1;
     }
-
+    
     ping(infoptr->ai_addr);
     return 0;
 }
